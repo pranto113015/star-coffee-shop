357422a407483a71c4f5ba63e56d08887302fd00
pranto113015 <pranto3876@gmail.com>
2025-11-25 15:34:08 +0600

    Add copyright section and update styles for header and navbar

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-let search= document.querySelector('.search-box');
-let searchIcon= document.querySelector('#search-icon');
+let search = document.querySelector(".search-box");
+let searchIcon = document.querySelector("#search-icon");
 
-searchIcon.onclick = ()=>{
-    search.classList.toggle('active');
-}
+searchIcon.onclick = () => {
+  search.classList.toggle("active");
+  navbar.classList.remove("active");
+};
+
+let navbar = document.querySelector(".navbar");
+let menuIcon = document.querySelector("#menu-icon");
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle("active");
+  search.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  search.classList.remove("active");
+};
+
+let header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
